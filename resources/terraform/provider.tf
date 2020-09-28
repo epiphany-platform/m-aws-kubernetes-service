d65895828363be3b6307723b0582d02f7be8483d
atsikham <atsikham@users.noreply.github.com>
2020-09-28 16:17:36 +0200

    Do not pass default region as a variable

--- resources/terraform/provider.tf
+++ resources/terraform/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = var.region
+  # No need to pass it as a variable as region is defined in m-aws-basic-infrastructure
+  # and this default must be set to some value
+  region = "eu-central-1"
 }
