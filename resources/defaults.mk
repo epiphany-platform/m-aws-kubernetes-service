732e608edc77f9e5f9ab3cabe20e5654d48275c9
atsikham <atsikham@users.noreply.github.com>
2020-09-30 17:04:08 +0200

    Removed kubeconfig generation and integrated with awsbi module

--- resources/defaults.mk
+++ resources/defaults.mk
@@ -1,10 +1,9 @@
 M_NAME ?= epiphany
 M_VPC_ID ?= unset
+M_PUBLIC_SUBNET_ID ?= unset
 M_K8S_VERSION ?= 1.17
 M_K8S_AUTOSCALER_VERSION ?= v1.17.3
 M_REGION ?= eu-central-1
-# Needs to be passed from basic infrastructure module
-M_PUBLIC_SUBNET_ID ?= unset
 
 # aws credentials
 M_AWS_ACCESS_KEY ?= unset
--- resources/terraform/modules/awsks/provider.tf
+++ resources/terraform/modules/awsks/provider.tf
@@ -3,8 +3,17 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  load_config_file       = "false"
   host                   = data.aws_eks_cluster.cluster.endpoint
+  load_config_file       = false
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    load_config_file       = false
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
 }
